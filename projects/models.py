31722aef3578c507ae9c693fe67ea618a4bf00d7
Sowayi <sapwao@gmail.com>
2019-06-29 10:27:02 -0400

    Add Project model to create instances of a project to be stored in the database

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
+class Project(models.Model):
+    """
+    class Project to create instances of projects uploaded to the site
+    """
+    
+    project_title=models.CharField(max_length=200)
+    project_image=models.ImageField(upload_to='projects/')
+    project_description=HTMLField()
+    live_link=models.URLField()
+    upload_date=models.DateTimeField(auto_now_add=True)
+    
+    
