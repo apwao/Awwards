097f0b19f7179109360b0f8495791ead689de23b
Sowayi <sapwao@gmail.com>
2019-06-29 12:17:46 -0400

    Add static url pattern for uploading images

--- projects/urls.py
+++ projects/urls.py
@@ -7,3 +7,5 @@ urlpatterns=[
     url('^$',views.homepage, name='homepage'),
     url(r'^upload_project/$', views.upload_project, name='upload_project')
 ]
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
