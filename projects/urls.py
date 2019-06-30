bfd9f798da702d19830f85d61bd363314d112650
Sowayi <sapwao@gmail.com>
2019-06-29 22:22:13 -0400

    Fix absent trailing slash in the view_projects url

--- projects/urls.py
+++ projects/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 urlpatterns=[
     url('^$',views.homepage, name='homepage'),
-    url(r'^upload_project/$', views.upload_project, name='upload_project')
+    url(r'^upload_project/$', views.upload_project, name='upload_project'),
+    url(r'^view_projects', views.view_projects, name='view_projects')
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
