2442b45b05a04f5f9d4865487de994de27ffed9f
Sowayi <sapwao@gmail.com>
2019-06-29 22:20:00 -0400

    Add fields to accomodate rating of a project and url to the live project

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
+from url_or_relative_url_field.fields import URLOrRelativeURLField
 
 # Create your models here.
 class Project(models.Model):
@@ -11,7 +12,14 @@ class Project(models.Model):
     project_title=models.CharField(max_length=200)
     project_image=models.ImageField(upload_to='projects/')
     project_description=HTMLField()
-    live_link=models.URLField()
+    live_link=URLOrRelativeURLField()
     upload_date=models.DateTimeField(auto_now_add=True)
+    posted_by=models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    usability_rating=models.IntegerField(default=0)
+    design_rating=models.IntegerField(default=0)
+    content_rating=models.IntegerField(default=0)  
+    posted_by=models.ForeignKey(User, null=True, on_delete=models.CASCADE) 
+    
+    
     
     
