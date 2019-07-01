fa9ff696e31342f1a166bf1afddc5dbe8896c13d
Sowayi <sapwao@gmail.com>
2019-07-01 01:08:59 -0400

    Import Rating and RatingForm classes to enable rating within the projects app

--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from .forms import ProjectForm
 from .models import Project
+from rating.models import Rating
+from rating.forms import RatingForm
 from django.contrib.auth.decorators import login_required
+from rating.views import rate_project,rate_form
+from rating import *
 
 
 # Create your views here.
@@ -14,35 +18,31 @@ def homepage(request):
     """
     
     return render(request, 'homepage.html')
-@login_required(login_url='/accounts/login')
+
+@login_required(login_url='/accounts/login/')
 def upload_project(request):
     """
     upload_project view function that presents a form to the user for
     providing details about their project and uploading them onto the application 
     for vetting
     """
-    print('hello')
     current_user = request.user
-    print(current_user)
     current_user_name = current_user.username
-    print(current_user_name)
+    # project_ratings=Rating.objects.filter(id=project_id)
     if request.method == 'POST':
-        print('True')
         form = ProjectForm(request.POST, request.FILES)
-        print(form)
         if form.is_valid():
-            print('---' * 30)
-            print('True')
-            project_post = form.save(commit=True)
-            print('**' * 20)
-            print(project_post)
-        return redirect('view_projects')
+            project_post = form.save(commit=True)  
+        else:
+            raise Http404 
+            
+        return redirect(view_projects)
     else:   
         project_form=ProjectForm()
         
-    return render(request, 'upload_project.html', {'project_form':project_form,"current_user_name":current_user_name})
+    return render(request, 'upload_project.html', {'project_form':project_form})
 
-@login_required(login_url='/accounts/login')
+@login_required(login_url='/accounts/login/')
 def view_projects(request):
     """
     view_project view function to display the projects after the user 
@@ -52,4 +52,28 @@ def view_projects(request):
     current_user_name=current_user.username
     projects=Project.objects.all()
     return render(request, 'view_projects.html',{'projects':projects, 'current_user_name':current_user})
+
+@login_required(login_url='/accounts/login/')
+def single_project(request,project_id):
+    """
+    single_project view function to display the details of a single project and display a form to
+    submit a vote for the project
+    """
+    
+    project=Project.objects.get(id=project_id)
+    
+    
+    
+    return render(request, 'single_project.html',{'project':project})
+
+
+@login_required(login_url='/accounts/login/')
+def vote(request):
+    """
+    vote view function to process the input from the vote form and return the average vote
+    """
+    overall_vote = rate_project()
+
+    
+    return render(request, 'rateform.html')
     
