1270e540582b0d60dc47bcea2c7b6fbf94c995b2
Sowayi <sapwao@gmail.com>
2019-07-01 01:23:29 -0400

    Add url for rating a project

---
+++ rating/urls.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.conf.urls import url
+from . import views
+from django.conf.urls.static import static
+
+urlpatterns=[
+    
+    url(r'^rate_form/',views.rate_form, name='rate_form'),
+    url(r'^rate_project/(\d+)/',views.rate_project, name='rate_project'),
+]
