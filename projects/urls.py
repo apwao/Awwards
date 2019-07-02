bee3096e40c73dcdb48ab7684ffc6bdf34bfd578
Sowayi <sapwao@gmail.com>
2019-07-02 17:28:34 -0400

    Add urls for a single project, search functionality and projects api

--- projects/urls.py
+++ projects/urls.py
@@ -7,7 +7,9 @@ urlpatterns=[
     url('^$',views.homepage, name='homepage'),
     url(r'^upload_project/$', views.upload_project, name='upload_project'),
     url(r'^view_projects/$', views.view_projects, name='view_projects'),
-    url(r'^single_project/(\d+)/', views.single_project, name='single_project')
+    url(r'^single_project/(\d+)/', views.single_project, name='single_project'),
+    url(r'^search/', views.search, name='search'),
+    url(r'^api/projects/$', views.ProjectList.as_view()),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
