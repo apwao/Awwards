fa8df0261d39cdf3543077204640c773aeb561fa
Sowayi <sapwao@gmail.com>
2019-07-02 17:29:04 -0400

    Import modules required for the correct functioning of an API

--- projects/views.py
+++ projects/views.py
@@ -7,7 +7,12 @@ from rating.models import Rating
 from rating.forms import RatingForm
 from django.contrib.auth.decorators import login_required
 from rating.views import rate_project,rate_form
-from rating import *
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import  Project
+from .serializer import ProjectSerializer
+from rest_framework import status
+
 
 
 # Create your views here.
@@ -59,21 +64,55 @@ def single_project(request,project_id):
     single_project view function to display the details of a single project and display a form to
     submit a vote for the project
     """
-    
     project=Project.objects.get(id=project_id)
+    # get all ratings for a specific project
+    all_ratings=Rating.objects.filter(project_id=project)
+    average_rates=[]
+    overall_final_rating=0
+        
+    for rating in all_ratings:
+        average_rates.append(rating.average_vote)
+            
+    final_rates=sum(average_rates)
+    if len(all_ratings)>0:
+        overall_rating=final_rates/len(all_ratings)
+        overall_final_rating=overall_rating
+    else:
+        overall_rating=0
+        overall_final_rating=overall_rating  
     
-    
-    
-    return render(request, 'single_project.html',{'project':project})
+    return render(request, 'single_project.html',{'project':project,'overall_final_rating':overall_final_rating})
 
 
-@login_required(login_url='/accounts/login/')
-def vote(request):
+def search(request):
     """
-    vote view function to process the input from the vote form and return the average vote
+    search function to search the database using a search term
+    and return the project.
     """
-    overall_vote = rate_project()
+    if 'find_project' in request.GET and request.GET['find_project']:
+        project_name=request.GET.get('find_project')
+        
+        searched_project=Project.search_project(project_name)
+        
+    return render(request,'search_results.html',{'searched_project':searched_project})  
 
+class ProjectList(APIView):
+    """
+    """
+    def get(self,request, format=None):
+        """
+        method get to perform GET operations
+        """
+        all_projects=Project.objects.all()
+        serializers = ProjectSerializer(all_projects, many=True)
+        return Response(serializers.data)
     
-    return render(request, 'rateform.html')
-    
+    def post(self, request, formal=None):
+        """
+        method post used to perform POST operations
+        """
+        serializers = ProjectSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
