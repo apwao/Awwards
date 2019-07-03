90e2f50b504638667dd74b0345b4807f01ab9e36
Sowayi <sapwao@gmail.com>
2019-07-03 07:05:46 -0400

    Fix imports to enable access to static files

--- awwards/urls.py
+++ awwards/urls.py
@@ -17,7 +17,8 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.contrib.auth import views
 from rest_framework.authtoken.views import obtain_auth_token
-from . import settings
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
